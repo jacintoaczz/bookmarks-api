69c873476ef1f968dfd41bdb2e23f124ff21ca66
Jacinto Acosta <jacintoaczz@gmail.com>
2022-05-17 21:00:47 -0400

    feat: Add Postgres with Docker
    * Configure Prisma
    * Create basic models
    * Add new auth dtos
    * Add new modules

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private _prisma: PrismaService){}
+
   signup() {
     return { msg: 'I have sign up!' };
   }
